3d0af40144adf1addd20e22ab6dc804127e56236
Lucas MG <lu.mang.gon@gmail.com>
2022-07-30 20:36:29 -0300

    created 2nd API server

--- src/app/components/bookstore-app/product-list/service/product-list.component.service.ts
+++ src/app/components/bookstore-app/product-list/service/product-list.component.service.ts
@@ -5,7 +5,11 @@ import { HttpClient, HttpHeaders } from "@angular/common/http";
 
 export class BookService {
 
-    private url = 'https://sheet.best/api/sheets/efcdb0bc-4bcb-4f05-ab91-6f0e4845d234';
+    private url = 'https://sheet.best/api/sheets/91a681be-eaed-4e37-a3d9-d23fd9f4d3c4';
+
+    // URL para conectar em API caso limite de Requests seja atingido
+    // https://sheet.best/api/sheets/efcdb0bc-4bcb-4f05-ab91-6f0e4845d234
+    // https://sheet.best/api/sheets/91a681be-eaed-4e37-a3d9-d23fd9f4d3c4
 
     httpOptions = {
         Headers: new HttpHeaders({ 'content-type': 'application/json' })
