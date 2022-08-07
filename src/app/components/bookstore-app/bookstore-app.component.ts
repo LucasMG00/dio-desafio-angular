ece9ff2f95a5ee5dbacc3fdd52ea38d3e8652edb
Lucas MG <lu.mang.gon@gmail.com>
2022-08-06 22:23:43 -0300

    fixed double api request

--- src/app/components/bookstore-app/bookstore-app.component.ts
+++ src/app/components/bookstore-app/bookstore-app.component.ts
@@ -9,39 +9,12 @@ import { BookService } from './product-list/service/product-list.component.servi
 })
 export class BookstoreAppComponent implements OnInit {
 
-  bookService: BookService
-  livros: any;
-  
-  filteredBooks: Book[] = [];
-
-    _books: Book[] = [];
-
-    _filterBy: string = '';
-
-    constructor(bookService: BookService) {
-      this.bookService = bookService;
+    constructor() {
+      
      }
   
     ngOnInit(): void {
-      this.retrieveAll()
+      
     }
-
-    retrieveAll(): void {
-      this.livros = this.bookService.getBook().subscribe((data => {
-        this.livros = data;
-        console.log(this.livros)
-      }))
-    }
-
-  set filter(value: string) {
-    this._filterBy = value;
-
-    this.filteredBooks = this._books.filter((book: Book) => 
-    book.name.toLocaleLowerCase()
-    .indexOf(this._filterBy.toLocaleLowerCase()) > -1);
-}
-get filter() {
-    return this._filterBy;
-}
 
 }
